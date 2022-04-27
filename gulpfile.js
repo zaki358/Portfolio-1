c8fee7848186f0a3226399c47a20df52184388ff
zaki358 <t.yamazaki.1129@gmail.com>
2022-04-27 23:21:00 +0900

    準備

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+// gulpプラグインの読み込み
+const gulp = require("gulp");
+// Sassをコンパイルするプラグインの読み込み
+const sass = require("gulp-sass")(require("sass"));
+
+// style.scssをタスクを作成する
+gulp.task("default", function() {
+  // style.scssファイルを取得
+  return (
+    gulp
+      .src("scss/style.scss")
+      // Sassのコンパイルを実行
+      .pipe(sass())
+      // cssフォルダー以下に保存
+      .pipe(gulp.dest("css"))
+  );
+});
