1bb0a8e2ec983845c1b6f492e674a0b8a4ef95c6
zaki358 <t.yamazaki.1129@gmail.com>
2022-04-30 00:35:33 +0900

    gulp構築修正とフロントページのマークアップ

--- gulpfile.js
+++ gulpfile.js
@@ -8,17 +8,29 @@ const webpackStream = require("webpack-stream");
 const webpack = require("webpack");
 const webpackConfig = require("./webpack.config");
 
-// style.scssをタスクを作成する
-// style.scssファイルを取得
-gulp.task("default",  () => {
-    return (
-        gulp
-            .src("scss/style.scss")
-            // Sassのコンパイルを実行
-            .pipe(sass())
-            // cssフォルダー以下に保存
-            .pipe(gulp.dest("css"))
-    );
+// style.scssの監視タスクを作成する
+gulp.task("default", function () {
+    // ★ style.scssファイルを監視
+    return gulp.watch("css/style.scss", function () {
+        // style.scssの更新があった場合の処理
+
+        // style.scssファイルを取得
+        return (
+            gulp
+                .src("css/style.scss")
+                // Sassのコンパイルを実行
+                .pipe(
+                    sass({
+                        outputStyle: "expanded"
+                    })
+                        // Sassのコンパイルエラーを表示
+                        // (これがないと自動的に止まってしまう)
+                        .on("error", sass.logError)
+                )
+                // cssフォルダー以下に保存
+                .pipe(gulp.dest("css"))
+        );
+    });
 });
 
 // webpackのタスクの定義。
