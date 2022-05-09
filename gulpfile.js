79a55de9cff6c301506e70521bda45065041e403
zaki358 <t.yamazaki.1129@gmail.com>
2022-05-09 23:09:39 +0900

    20220509更新-1

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,24 @@
-// gulpプラグインの読み込み
-// Sassをコンパイルするプラグインの読み込み
-// webpackをコンパイルするプラグインの読み込み
-// webpackの設定ファイルの読み込み
-// autoprefixerの読み込み
+//モジュールの読み込み
 const gulp = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
+const { src, dest, watch, series, parallel } = require("gulp");
+const sassGlob = require("gulp-sass-glob-use-forward");
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require("gulp-autoprefixer");
 const webpackStream = require("webpack-stream");
 const webpack = require("webpack");
 const webpackConfig = require("./webpack.config");
-const autoprefixer = require('gulp-autoprefixer');
 
-//style.scssの監視タスクを作成する
-gulp.task("default", function () {
-    return gulp.watch("css/style.scss", function () {
-        return (gulp.src("css/style.scss")
-                .pipe(sass({outputStyle: "expanded"}).on("error", sass.logError))
-                .pipe(autoprefixer({  //autoprefixerの実行
-                    browsers: ["last 2 versions"],
-                    cascade: false
-                }))  
-                .pipe(gulp.dest("css"))
-        );
-    });
-});
+//関数定義
+function compile(done) {
+    src("./sass/**/*.scss")
+        .pipe(sassGlob())
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(dest("./css"));
+    done();
+}
+
+exports.compile = series(compile);
 
 // webpackのタスクの定義。
 gulp.task("default", () => {
@@ -30,3 +26,18 @@ gulp.task("default", () => {
     return webpackStream(webpackConfig, webpack)
         .pipe(gulp.dest("dist"));
 });
+
+//後ほど削除↓
+// //style.scssの監視タスクを作成する
+// gulp.task("default", function () {
+//     return gulp.watch("css/style.scss", function () {
+//         return (gulp.src("css/style.scss")
+//                 .pipe(sass({outputStyle: "expanded"}).on("error", sass.logError))
+//                 .pipe(autoprefixer({  //autoprefixerの実行
+//                     browsers: ["last 2 versions"],
+//                     cascade: false
+//                 }))
+//                 .pipe(gulp.dest("css"))
+//         );
+//     });
+// });
