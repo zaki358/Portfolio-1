32ba4e2397288cb4f91675c223ac1a46547bdb4c
zaki358 <t.yamazaki.1129@gmail.com>
2022-05-11 22:27:33 +0900

    JS編集

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,1 @@
-window.addEventListener("resize",(()=>{let e=window.innerWidth,n=window.innerHeight;const t=document.querySelector("#l-container");t.style.width=e+"px",t.style.height=n+"px",console.log(n)}));
+!function(){const e=()=>{let e=window.innerWidth,n=window.innerHeight;const t=document.querySelector("#l-container");t.style.width=e+"px",t.style.height=n+"px"};window.addEventListener("resize",e),window.addEventListener("load",e)}();
--- script/main.js
+++ script/main.js
@@ -1,10 +1,11 @@
-window.addEventListener('resize', () => {
+const screenSize = () => {
     let window_w = window.innerWidth;
     let window_h = window.innerHeight;
     const container = document.querySelector("#l-container");
     container.style.width = window_w + "px";
     container.style.height = window_h + "px";
-    console.log(window_h);
-});
+};
+window.addEventListener('resize', screenSize);
+window.addEventListener('load',screenSize);
 
 
