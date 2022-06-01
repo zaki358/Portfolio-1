d3ebcc2f0a5a7b9d93aa6ee9081c94da9d1c023f
zaki358 <t.yamazaki.1129@gmail.com>
2022-06-01 23:30:12 +0900

    CSS & JS 修正

--- script/main.js
+++ script/main.js
@@ -1,22 +1,10 @@
-
-
-const screenSize = (w, h) => {
-    const container = document.querySelector("#l-container");
-    container.style.width = w + "px";
-    container.style.height = h + "px";
-    return {
-        a: w,
-        b: h
-    };
-};
-
 let angle = 90;
 
 const circumference = (hhh) => {
-    let moveElement = document.querySelector('.c-circle__title');
+    let moveElement = document.querySelector('.p-area__rotate');
     let radius = (hhh * 0.7) / 2;
-    let centerX = radius - 10;
-    let centerY = radius - 10;
+    let centerX = radius - 75;
+    let centerY = radius - 75;
     let moveX = Math.cos(Math.PI / 180 * angle) * radius + centerX;
     let moveY = Math.sin(Math.PI / 180 * angle) * radius + centerY;
     moveElement.style.left = moveX + "px";
@@ -26,33 +14,25 @@ const circumference = (hhh) => {
     } else {
         angle = 0;
     }
-    //console.log(radius);
 }
 
 setInterval(() => {
-    let window_w = window.innerWidth;
     let window_h = window.innerHeight;
-    let { a, b } = screenSize(window_w, window_h);
-
-    circumference(b);
-},1000);
+    circumference(window_h);
+},20);
 
 
-// window.addEventListener('resize',() => {
-//     let window_w = window.innerWidth;
-//     let window_h = window.innerHeight;
-//     let{a,b} = screenSize(window_w,window_h);
+// const screenSize = (w, h) => {
+//    const container = document.querySelector("#l-container");
+//    container.style.width = w + "px";
+//    container.style.height = h + "px";
+//    return {
+//        a: w,
+//        b: h
+//    };
+// };
 
-//     circumference(b);
-// });
+//window.addEventListener('load', screenSize);
 
 
-window.addEventListener('load', screenSize);
-
-
-
-    // let window_w = window.innerWidth;
-    // let window_h = window.innerHeight;
-
-    //let mainRaius = window_h * 0.7;
 
