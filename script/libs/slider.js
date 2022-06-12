6bb2264632498d98f12ece3f7827e3c4e2f465e5
zaki358 <t.yamazaki.1129@gmail.com>
2022-06-12 23:20:19 +0900

    swiper

---
+++ script/libs/slider.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded",function(){
+   const WorkSlider = new slider(".swiper");
+   //WorkSlider.start();
+});
+
+class slider {
+   constructor(el) {
+      this.el = el;
+      this.swiper = this._initSwiper();
+   }
+   _initSwiper() {
+      return new Swiper(this.el, {
+         // Optional parameters
+         effect: "flip",
+         grabCursor: true,
+         // direction: 'vertical',
+         mousewheel: true,
+         loop: true,
+         centeredSlider: true,
+         sliderPerView: 1,
+         speed: 1000,
+         // autoplay: {
+         //    delay: 4000,
+         //    disableOnInteraction: false
+         // },
+       
+         // If we need pagination
+         pagination: {
+           el: '.swiper-pagination',
+         },
+       
+         // Navigation arrows
+         navigation: {
+           nextEl: '.swiper-button-next',
+           prevEl: '.swiper-button-prev',
+         },
+       
+         // And if we need scrollbar
+         scrollbar: {
+           el: '.swiper-scrollbar',
+         },
+       });
+   }
+   
+   //引数は空のオブジェクト
+   start(options = {}) {
+      options = Object.assign({
+          delay: 4000,
+          disableOnInteraction: false
+      }, options);
+      
+      this.swiper.params.autoplay = options;
+      this.swiper.autoplay.start();
+  }
+  stop() {
+   this.swiper.autoplay.stop();
+}
+}
