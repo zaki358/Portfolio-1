7b6e136caa7dd968c3008fb3cff5fa052ef02fcd
zaki358 <t.yamazaki.1129@gmail.com>
2022-07-15 01:14:29 +0900

    回転js作成

--- script/rotate.js
+++ script/rotate.js
@@ -65,6 +65,9 @@ const pointA= (hhh) => {
    let x = Math.cos(Math.PI / 180 * angle3) * radius + centerX;
    let y = Math.sin(Math.PI / 180 * angle3) * radius + centerY;
 
+   x = x + (hhh*0.14);
+   y = y - (hhh*0.07);
+
    return {
       left:x,
       top:y
@@ -83,6 +86,9 @@ const pointB= (hhh) => {
    let x = Math.cos(Math.PI / 180 * angle3) * radius + centerX;
    let y = Math.sin(Math.PI / 180 * angle3) * radius + centerY;
 
+   x = ((hhh * 0.7) / 2) - (hhh*0.07) ;
+   y = (hhh * 0.7) + (hhh*0.07);
+
    return {
       left:x,
       top:y
@@ -92,7 +98,7 @@ const pointB= (hhh) => {
 //150-270
 const pointC= (hhh) => {
    //角度210°
-   let angle3 = 90;
+   let angle3 = 210;
    //0.7は円の大きさ(70vh)→円の半径
    let radius = (hhh * 0.7) / 2;
    //0.14はmenuの円の大きさ(14vh)
@@ -100,7 +106,8 @@ const pointC= (hhh) => {
    let centerY = radius - ((hhh*0.14)/2);
    let x = Math.cos(Math.PI / 180 * angle3) * radius + centerX;
    let y = Math.sin(Math.PI / 180 * angle3) * radius + centerY;
-
+   x = x - (hhh*0.14);
+   y = y - (hhh*0.14);
    return {
       left:x,
       top:y
@@ -115,13 +122,14 @@ const A = pointA(window_h);
 const B = pointB(window_h);
 const C = pointC(window_h);
 
-
+//座標
 const left30 = get30.left;
 const top30 = get30.top;
 const left150 = get150.left;
 const top150 = get150.top;
 const left270 = get270.left;
 const top270 = get270.top;
+//制御点
 const leftA = A.left;
 const topA = A.top;
 const leftB = B.left;
@@ -129,19 +137,116 @@ const topB = B.top;
 const leftC = C.left;
 const topC = C.top;
 
-console.log(left30);
-console.log(top30);
-console.log(left150);
-console.log(top150);
-console.log(left270);
-console.log(top270);
+console.log("left30の値",left30);
+console.log("top30の値",top30);
+console.log("left150の値",left150);
+console.log("top150の値",top150);
+console.log("left270の値",left270);
+console.log("top270の値",top270);
 console.log("要素の高さ",window_h);
-console.log(leftA);
-console.log(topA);
-console.log(leftB);
-console.log(topB);
-console.log(leftC);
-console.log(topC);
+console.log("leftAの値",leftA);
+console.log("topAの値",topA);
+console.log("leftBの値",leftB);
+console.log("topBの値",topB);
+console.log("leftCの値",leftC);
+console.log("topCの値",topC);
+
+//二次ベジェ曲線算出
+function moveCurve(t, startX, startY, endX, endY, controlX, controlY) {
+   let tp = 1 - t;
+   let moveLeft = (tp ** 2 * startX) + (2 * tp * t * controlX) + (t ** 2 * endX );
+   let moveTop = (tp ** 2 * startY) + (2 * tp * t * controlY) + (t ** 2 * endY );
+   return {
+      left:moveLeft,
+      top:moveTop
+   }
+}
+
+
+let progress = 0;
+let time = 3000;
+
+
+
+const scrollEvent = function () {
+   window.addEventListener("wheel", function () {
+       // ここに処理を書いていく
+       requestAnimationFrame(update1);
+      requestAnimationFrame(update2);
+      requestAnimationFrame(update3);
+   });
+};
+
+scrollEvent();
+   //window.addEventListener('scroll',function(){
+      
+   //});
+
+
+
+function update1(timestamp) {
+   progress = timestamp / time;
+   progress = Math.min(progress, 1);
+   //console.log(progress);
+   let moveElement = document.querySelector('.p-area__front-menu--lv1');
+   if (progress >= 0) {
+
+      let tp = 1 - progress;
+      let moveLeft = (tp ** 2 * left30) + (2 * tp * progress * leftB) + (progress ** 2 * left150 );
+      let moveTop = (tp ** 2 * top30) + (2 * tp * progress * topB) + (progress ** 2 * top150 );
+
+      //moveCurve(progress, left30, top30, left150, top150, leftB, topB);
+      //console.log("移動距離",moveLeft);
+      moveElement.style.left = moveLeft + "px";
+      moveElement.style.top = moveTop + "px";
+   }
+   if (progress < 1){
+      requestAnimationFrame(update1);
+   }
+}
+
+function update2(timestamp) {
+   progress = timestamp / time;
+   progress = Math.min(progress, 1);
+   //console.log(progress);
+   let moveElement = document.querySelector('.p-area__front-menu--lv2');
+   if (progress >= 0) {
+
+      let tp = 1 - progress;
+      let moveLeft = (tp ** 2 * left150) + (2 * tp * progress * leftC) + (progress ** 2 * left270 );
+      let moveTop = (tp ** 2 * top150) + (2 * tp * progress * topC) + (progress ** 2 * top270 );
+
+      //moveCurve(progress, left30, top30, left150, top150, leftB, topB);
+      //console.log("移動距離",moveLeft);
+      moveElement.style.left = moveLeft + "px";
+      moveElement.style.top = moveTop + "px";
+   }
+   if (progress < 1){
+      requestAnimationFrame(update2);
+   }
+}
+
+function update3(timestamp) {
+   progress = timestamp / time;
+   progress = Math.min(progress, 1);
+   //console.log(progress);
+   let moveElement = document.querySelector('.p-area__front-menu--lv3');
+   if (progress >= 0) {
+
+      let tp = 1 - progress;
+      let moveLeft = (tp ** 2 * left270) + (2 * tp * progress * leftA) + (progress ** 2 * left30 );
+      let moveTop = (tp ** 2 * top270) + (2 * tp * progress * topA) + (progress ** 2 * top30 );
+
+      //moveCurve(progress, left30, top30, left150, top150, leftB, topB);
+      //console.log("移動距離",moveLeft);
+      moveElement.style.left = moveLeft + "px";
+      moveElement.style.top = moveTop + "px";
+   }
+   if (progress < 1){
+      requestAnimationFrame(update3);
+   }
+}
+
 
 
 
